154fc1ff87cc8c93e4fb423ad893fcb807134a34
Kry765 <krzysztof.kleka@poczta.onet.pl>
2022-10-15 17:53:17 +0200

    add burger-icon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
 	<nav>
-		<div class="top-nav flex-center-position burger-icon">
+		<div class="top-nav flex-center-position">
+			<div><img src="../picture/menu.png" alt="menu" class="burger-icon" /></div>
 			<div><a class="top-nav-element" href="#home">Strona główna</a></div>
 			<div><a class="top-nav-element" href="#aboutme">O nas</a></div>
 			<div><a class="top-nav-element" href="#goals">Osiągnięcia</a></div>
