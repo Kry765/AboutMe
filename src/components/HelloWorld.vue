d825ddc8d5dc1a1f500e462673d69c86edb55bc0
Kry765 <krzysztof.kleka@poczta.onet.pl>
2022-10-15 20:00:00 +0200

    delete br

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,15 +8,21 @@
 			<div><a class="top-nav-element" href="#contact">Kontakt</a></div>
 		</div>
 	</nav>
-	<header>
+	<header id="home">
 		<div class="header-background flex-center-position">
 			<div class="shadow-header-background"></div>
 			<div class="welcome-text">
 				<h1>Witaj, chcesz nieco więcej dowiedzieć się o mnie?</h1>
-				<button type="button" class="welcome-button">Przejdź dalej</button>
+				<br />
+				<a href="#aboutme" class="welcome-button">Przejdź dalej</a>
 			</div>
 		</div>
 	</header>
+	<main>
+		<section>
+			<div class="about-me" id="aboutme"></div>
+		</section>
+	</main>
 </template>
 <style scoped lang="scss">
 @import '../scss/style.scss';
