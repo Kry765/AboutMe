8a9273d540f8a62590ebd5145a0556fb2b4a6959
Kry765 <krzysztof.kleka@poczta.onet.pl>
2022-10-15 16:48:53 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+	<nav>
+		<div class="top-navigation">
+			<a href="#home">Strona główna</a>
+			<a href="#aboutme">O nas</a>
+			<a href="#goals">Osiągnięcia</a>
+			<a href="#skills">Umiejętności</a>
+			<a href="#contactme">Kontakt</a>
+		</div>
+	</nav>
+</template>
+
+<script></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
