fcf03d0ac23e1a53fe17a60331c8f86f2eba994b
Kry765 <krzysztof.kleka@poczta.onet.pl>
2022-10-21 22:39:56 +0200

    add new icon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
 	<nav>
 		<div class="top-nav flex-end-position">
+			<div class="burger-top-menu">
+				<ion-icon name="menu-sharp"></ion-icon>
+			</div>
 			<div><a class="top-nav-element" href="#home">Strona główna</a></div>
 			<div><a class="top-nav-element" href="#aboutme">O mnie</a></div>
 			<div><a class="top-nav-element" href="#goals">Osiągnięcia</a></div>
@@ -20,7 +23,29 @@
 	</header>
 	<main>
 		<section>
-			<div class="about-me" id="aboutme"></div>
+			<div class="skills">
+				<h1>Umiejętności</h1>
+				<div class="html">
+					<div class="html-card">
+						<ion-icon name="airplane-outline"></ion-icon>
+					</div>
+				</div>
+				<div class="css">
+					<div class="css-card"></div>
+				</div>
+				<div class="javascript">
+					<div class="javascript card"></div>
+				</div>
+				<div class="github">
+					<div class="github-card"></div>
+				</div>
+				<div class="scss">
+					<div class="scss-card"></div>
+				</div>
+				<div class="vue">
+					<div class="vue-card"></div>
+				</div>
+			</div>
 		</section>
 	</main>
 </template>
